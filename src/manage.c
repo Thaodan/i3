3b7dba65c3d306f01b3ce91f3b407c968ab34732
Daniel Selinger <daniel@slite.org>
2014-08-30 13:48:46 +0200

    Handle windows with _NET_WM_STATE_STAYS_ON_TOP window state such as popups
    Popups for example from knotify, krunner, networkmanager, panel configuration and alike
    
    Co-authored-by: Marius Muja <mariusm@cs.ubc.ca>
    Co-authored-by: Björn Bidar <bjorn.bidar@thaodan.de>

--- src/manage.c
+++ src/manage.c
@@ -245,6 +245,18 @@ void manage_window(xcb_window_t window, xcb_get_window_attributes_cookie_t cooki
     /* Where to start searching for a container that swallows the new one? */
     Con *search_at = croot;
 
+    bool haswm_type_ontop = false;
+    bool haswm_type_dock = false;
+    if (xcb_reply_contains_atom(state_reply, A__NET_WM_STATE_STAYS_ON_TOP)) {
+        /* this is a window like for example a popup */
+        haswm_type_ontop = true;
+        if (xcb_reply_contains_atom(type_reply, A__NET_WM_WINDOW_TYPE_DOCK)) {
+            /* kde _NET_WM_STATE_STAYS_ON_TOP often are
+               _NET_WM_WINDOW_TYPE_DOCK while in reality they dont behave as docks */
+           haswm_type_dock = true;
+        }
+    }
+
     /* Ignore DESKTOP windows and move them to the bottom of the stack. */
     if (xcb_reply_contains_atom(type_reply, A__NET_WM_WINDOW_TYPE_DESKTOP)) {
         LOG("Ignoring window of type desktop\n");
@@ -522,6 +534,18 @@ void manage_window(xcb_window_t window, xcb_get_window_attributes_cookie_t cooki
      * here because it’s used for dock clients. */
     if (nc->geometry.width == 0)
         nc->geometry = (Rect){geom->x, geom->y, geom->width, geom->height};
+
+
+    if (haswm_type_ontop) {
+        /* we always want to _NET_WM_STATE_STAYS_ON_TOP be floating with no border */
+        nc->border_style = BS_NONE;
+        nc->border_width = 0;
+        /* pass false otherwise floating_enable will reset our border to the default
+           from the config */
+        floating_enable(nc, false);
+        /* we dont need to try to float this again */
+        want_floating = false;
+    }
 
     if (want_floating) {
         DLOG("geometry = %d x %d\n", nc->geometry.width, nc->geometry.height);
